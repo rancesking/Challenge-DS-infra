3c34ebb673b6b5eff357239b0a111de13d1593ac
Rances Acosta <rances.acosta21@gmail.com>
2023-09-15 12:27:49 -0400

    fixed typo on the k8s manifest

--- modules/vpc/sg.tf
+++ modules/vpc/sg.tf
@@ -16,6 +16,12 @@ resource "aws_security_group" "cluster_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
     ingress {
+    from_port   = 16443
+    to_port     = 16443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
